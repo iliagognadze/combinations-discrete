09ffef9b541b983d61d6ae2ca3ca79abb004deaf
iliagognadze <50463093+iliagognadze@users.noreply.github.com>
2022-01-18 23:23:06 +0400

    First Commit

---
+++ Tester.java
@@ -0,0 +1,15 @@
+
+package combinations;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String tt = "ALABAMA";
+		
+		System.out.println(Combinations.numberOfPossibleCombinations(tt.toCharArray()));
+		
+	}
+
+}
